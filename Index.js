1400282f53fabbc0cde4e0f10c29bd5d6c9c0255
toxidity-18 <desavigatunganga@gmail.com>
2025-06-09 14:20:11 +0300

    ...

---
+++ Index.js
@@ -0,0 +1,11 @@
+// 9 june 2025
+
+// find html elements in the render page 
+// remove and add new elements 
+// adjust properties of the selected element 
+
+
+// work in progress 
+document.querySelector(selector)
+
+// dom as an upside down tree structure 
