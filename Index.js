0c9b73b0ab801a2a823d94879402112e6d5bd5c2
toxidity-18 <desavigatunganga@gmail.com>
2025-06-09 21:58:42 +0300

    ...

--- Index.js
+++ Index.js
@@ -9,3 +9,17 @@
 document.querySelector(selector)
 
 // dom as an upside down tree structure 
+
+// 9 june 2025
+// think about a tree structure : where element are sub trees of one another .
+
+// finding elements via metadata : class and id
+// finding node : getElementByID , getElementByClassName , getElementByTagName 
+document.getElementById("greetings")   // <h1 id = "greetings" >Hello! ...
+
+// view:page-source ... html code for a website
+
+// HTML Collection : array like structure containing a list of elements  thus for loop is applied for array iteration 
+document.getElementsByClassName('banner')// <div class = 'banner' > ...
+
+// fast repeat the whole os js 
